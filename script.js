5fc4a8f73010868f808e6bd744eb2494ab19277e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-10-03 00:32:44 +0100

    2. Using Geolocation API (GoogleMaps ...)

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const {latitude, longitude} = position.coords
+            console.log('latitude :',latitude,' , ', 'longitude :', longitude)
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        },
+        function () {
+            alert('Could not get your position ...!')
+        })
+} else {
+    alert('Your browser is too old !')
+}
 
